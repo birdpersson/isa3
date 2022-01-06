dfe496a08cae607c423411f28f7d0a411f85e433
birdpersson <aleksandarvujasin@gmail.com>
2022-01-06 16:02:42 +0100

    Initial commit

---
+++ src/main/java/com/isa35/isa3/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.isa35.isa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
